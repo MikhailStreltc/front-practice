8f37a13d8554891a50d4460e655e8b8a8935e0ec
MikhailStreltc <mstreltc@gmail.com>
2024-01-15 17:29:49 +0300

    Reset annoying button

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,16 @@ function App() {
     setClicks(clicks + 1);
   }
 
+  function resetClicks() {
+    setClicks(0);
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
         <RedButton onClick={increaseClicks}>Do not Push!</RedButton>
         <Counter>И всё же ты нажал {clicks}</Counter>
+        <BlueButton onClick={resetClicks}>reset</BlueButton>
       </header>
     </div>
   );
@@ -44,4 +49,15 @@ const RedButton = styled.button({
 const Counter = styled.p({
   fontSize: 24,
   color: '#FFFFFF',
+});
+const BlueButton = styled.button({
+  height: 50,
+  width: 200,
+  backgroundColor: '#00FFFF',
+  color: '#FFFFFF',
+  fontSize: 15,
+  fontWeight: 700,
+  borderRadius: '33%',
+  border: '3px #FFFFFF',
+  cursor: 'pointer',
 });
