7ad20bacdf6fa5cc45160df790117196602232aa
MikhailStreltc <mstreltc@gmail.com>
2024-01-28 12:45:56 +0300

    Added back

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components';
 import './App.css';
+import { click } from '@testing-library/user-event/dist/click';
+
+interface ClicksData {
+  clicks: number;
+}
 
 function App() {
   const [clicks, setClicks] = React.useState(0);
 
+  React.useEffect(function () {
+    fetch('http://localhost:8000/clicks')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data: ClicksData) {
+        setClicks(data.clicks);
+      });
+  }, []);
+
   function increaseClicks() {
     setClicks(clicks + 1);
+
+    const data: ClicksData = {
+      clicks: clicks + 1,
+    };
+
+    fetch('http://localhost:8000/clicks', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
   }
 
   function resetClicks() {
@@ -18,7 +46,8 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <RedButton onClick={increaseClicks}>Do not Push!</RedButton>
-        <Counter>И всё же ты нажал {clicks} раз</Counter> {/* как сделать раз/разы */}
+        <Counter>И всё же ты нажал {clicks} раз</Counter>{' '}
+        {/* как сделать раз/разы */}
         <BlueButton onClick={resetClicks}>reset</BlueButton>
       </header>
     </div>
